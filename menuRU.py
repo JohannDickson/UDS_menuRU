0c92caf8f14736a18ef30661568bb5fc72f8e084
JohannDickson <johann.dickson@gmail.com>
2012-11-30 23:24:04 +0100

    Removed logging information

--- menuRU.py
+++ menuRU.py
@@ -1,16 +1,12 @@
 # Requires feedparser, icalendar, pytz
 
-import feedparser, pytz, logging
+import feedparser
+import pytz
 from icalendar import Calendar, Event
 from datetime import datetime
 
-logfile = "logs/menuRU.log"
 calName = "calendars/menuRU.ics"
-
-logging.basicConfig(filename=logfile,level=logging.INFO) # level can be 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
-logging.info("-----")
-logging.info(datetime.now())
-logging.info("Fetching this week's menu")
+print "Exporting calendar: "+calName
 
 # CROUS restaurants: http://www.crous-grenoble.fr/article-27-mg-3-mh-25-ms-1-menu-de-la-semaine.htm
 chautagne = "http://www.crous-grenoble.fr/rss-menu-7.htm"
@@ -24,11 +20,6 @@ for item in menuRSS["entries"]:
     service = title[1]
     date = datetime.strptime(title[2], "%d/%m/%Y")
     item["summary_detail"]["value"] = item["summary_detail"]["value"].replace('<br />\n', '\\n')
-    
-    logging.debug(item["title_detail"]["value"])
-    logging.debug("---")
-    logging.debug(item["summary_detail"]["value"])
-    logging.debug("-----------------------------\r\n")
 
     ev = Event()
 
@@ -43,12 +34,10 @@ for item in menuRSS["entries"]:
             ev.add('dtend', datetime(date.year,date.month,date.day,20,0,0,tzinfo=pytz.timezone("Europe/Paris")))
 
         ev.add('description', item["summary_detail"]["value"])
-
         cal.add_component(ev)
 
-logging.info("Saving to file: "+calName)
 f = open(calName, 'wb')
 f.write(cal.to_ical())
 f.close()
 
-logging.info("Done")
+print "Done!"
