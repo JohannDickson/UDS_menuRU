1b9881cf52c149019078349bce52ea889ad160e4
JohannDickson <johann.dickson@gmail.com>
2013-02-23 20:22:12 +0100

    Method for calendar generation

--- menuRU.py
+++ menuRU.py
@@ -16,38 +16,41 @@ except:
     raise
     sys.exit(0)
 
-calName = "calendars/menuRU.ics"
-print "Exporting calendar: "+calName
+def makeCal(outputFile, feed):
+    cal = Calendar()
 
-# CROUS restaurants: http://www.crous-grenoble.fr/article-27-mg-3-mh-25-ms-1-menu-de-la-semaine.htm
-chautagne = "http://www.crous-grenoble.fr/rss-menu-7.htm"
+    menuRSS = feedparser.parse(feed)
+    for item in menuRSS["entries"]:
+        title = item["title_detail"]["value"].split()
+        jour = title[0]
+        service = title[1]
+        date = datetime.strptime(title[2], "%d/%m/%Y")
+        item["summary_detail"]["value"] = item["summary_detail"]["value"].replace('<br />\n', '\\n')
 
-cal = Calendar()
+        ev = Event()
 
-menuRSS = feedparser.parse(chautagne)
-for item in menuRSS["entries"]:
-    title = item["title_detail"]["value"].split()
-    jour = title[0]
-    service = title[1]
-    date = datetime.strptime(title[2], "%d/%m/%Y")
-    item["summary_detail"]["value"] = item["summary_detail"]["value"].replace('<br />\n', '\\n')
+        if not ((jour=="Vendredi" and service=="soir") or jour=="Samedi" or jour=="Dimanche"):
+            ev.add('summary', 'menu RU')
+            if service == "midi":
+                ev.add('dtstart', datetime(date.year,date.month,date.day,11,30,0,tzinfo=timezone("Europe/Paris")))
+                ev.add('dtend', datetime(date.year,date.month,date.day,13,0,0,tzinfo=timezone("Europe/Paris")))
+            elif service == "soir":
+                ev.add('dtstart', datetime(date.year,date.month,date.day,18,30,0,tzinfo=timezone("Europe/Paris")))
+                ev.add('dtend', datetime(date.year,date.month,date.day,20,0,0,tzinfo=timezone("Europe/Paris")))
 
-    ev = Event()
+            ev.add('description', item["summary_detail"]["value"])
+            cal.add_component(ev)
 
-    if not ((jour=="Vendredi" and service=="soir") or jour=="Samedi" or jour=="Dimanche"):
-        ev.add('summary', 'menu RU')
-        if service == "midi":
-            ev.add('dtstart', datetime(date.year,date.month,date.day,11,30,0,tzinfo=timezone("Europe/Paris")))
-            ev.add('dtend', datetime(date.year,date.month,date.day,13,0,0,tzinfo=timezone("Europe/Paris")))
-        elif service == "soir":
-            ev.add('dtstart', datetime(date.year,date.month,date.day,18,30,0,tzinfo=timezone("Europe/Paris")))
-            ev.add('dtend', datetime(date.year,date.month,date.day,20,0,0,tzinfo=timezone("Europe/Paris")))
+    f = open(outputFile, 'wb')
+    f.write(cal.to_ical())
+    f.close()
 
-        ev.add('description', item["summary_detail"]["value"])
-        cal.add_component(ev)
+    print "Done!"
 
-f = open(calName, 'wb')
-f.write(cal.to_ical())
-f.close()
+if __name__=='__main__':
+    calName = "calendars/menuRU.ics"
+    print "Exporting calendar: "+calName
+    # CROUS restaurants: http://www.crous-grenoble.fr/article-27-mg-3-mh-25-ms-1-menu-de-la-semaine.htm
+    chautagne = "http://www.crous-grenoble.fr/rss-menu-7.htm"
 
-print "Done!"
+    makeCal(calName, chautagne)
